1c2de0fe985f8016489fdb304944db1a1fd9f7bf
Mahmoud Ahmed <mlartwel@gmail.com>
2020-05-21 14:48:37 +0200

    added netlify headers options

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,20 @@ module.exports = {
         display: 'swap'
       }
     },
-    `gatsby-plugin-netlify-headers`
+    {
+      resolve: `gatsby-plugin-netlify-headers`,
+      options: {
+        mergeSecurityHeaders: false,
+        headers: {
+          '/*': ['X-Frame-Options: sameorigin'],
+          "/**/*.html": ["cache-control: public, max-age=300"],
+          "/static/*": ["cache-control: public, max-age=31536000, immutable"],
+          "/*.js": ["cache-control: public, max-age=31536000, immutable"],
+          "/*.css": ["cache-control: public, max-age=31536000, immutable"],
+          "/sw.js": ["cache-control: public, max-age=0, must-revalidate"],
+        },
+      },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
